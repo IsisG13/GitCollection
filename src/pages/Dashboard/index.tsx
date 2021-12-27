0331600c4607c8c3299513733e7684a66442a680
IsisG13 <isissouzaguimaraes@gmail.com>
2021-12-27 16:07:39 -0300

    Estilizaçao do dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Title, Form } from './styles';
+import { FiChevronRight } from 'react-icons/fi'
+
+import { Title, Form, Repos } from './styles';
 import logo from '../../assets/logo.svg'
 
 export const Dashboard: React.FC = () => {
@@ -12,6 +14,21 @@ export const Dashboard: React.FC = () => {
                 <input placeholder="usermane/repository_name" />
                 <button type="submit">Buscar</button>
             </Form>
+
+            <Repos>
+                <a href="/repositories">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/94871953?s=400&u=25088c025fec67c83f39c4e19366bd05195f6000&v=4"
+                        alt="repositorio"
+                    />
+                    <div>
+                        <strong>IsisG13/GitCollection</strong>
+                        <p>Repositorio do mine curso gratuito de reactjs</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+
+            </Repos>
         </>
     )
 };
